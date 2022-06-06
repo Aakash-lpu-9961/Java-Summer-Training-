70ed1be27dc895b4acd27641863bc63b4c343f5a
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-06-06 21:44:04 +0530

    Receiving Input from User

--- src/com/company/RecievingInputFromUser.java
+++ src/com/company/RecievingInputFromUser.java
@@ -1,10 +1,14 @@
 package com.company;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.*;
 
 public class RecievingInputFromUser {
     public static void main(String[] args){
         try (Scanner sc = new Scanner(System.in)) {
             int a,b,c,d;
+            BigInteger z;
+            BigDecimal y;
             System.out.println("Enter the number 1:");
             a=sc.nextInt();
             System.out.println("Enter the number 2 :");
@@ -14,7 +18,28 @@ public class RecievingInputFromUser {
             d = a+b+c;
             System.out.println("Addition Result of "+a+" "+b+" "+c+" "+" is : "+d);
 
+            System.out.println("Enter the BigInteger :");
+            z=sc.nextBigInteger();
+            System.out.println(z);
+
+            System.out.println("Enter the BigDecimal :");
+            y=sc.nextBigDecimal();
+            System.out.println(y);
+
         }
     }
 
 }
+
+/*
+* Methods
+*       nextInt()
+*       nextFloat()
+*       nextDouble()
+*       nextByte()
+*       nextBoolean()
+*       nextLong()
+*       nextShort()
+*       nextBigInteger()
+*       nextBigDecimal()
+* */
