e6330b88d1c8c199c1c3606c8d1f7e1db34b0cd6
Aakash-lpu-9961 <aakashkumarjh9961@gmail.com>
2022-06-06 20:07:00 +0530

    Variables and Data Types

---
+++ src/com/company/VariablesDataTypes.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class VariablesDataTypes {
+    public static void main(String[] args){
+        System.out.println("Data Types in Java Programming");
+        System.out.println("==================================================");
+        System.out.println("Boolean Data Type :\t 1 Bit");
+        System.out.println("Char Data Type : \t 2 Byte");
+        System.out.println("Byte Data Type : \t 1 Byte");
+        System.out.println("Short Data Type : \t 2 Byte");
+        System.out.println("Int Data Type : \t 4 Bytes");
+        System.out.println("Long Data Type : \t 8 Byte");
+        System.out.println("Float Data Type : \t 4 Byte");
+        System.out.println("Double Data Type : \t 8 Byte");
+        System.out.println("===================================================");
+    }
+}
