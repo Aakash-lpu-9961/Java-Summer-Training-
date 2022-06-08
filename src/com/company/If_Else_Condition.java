df4727be04fabf9d2613495439fa7a43ec68892e
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-08 21:11:15 +0530

    If_Else_Condition

--- src/com/company/If_Else_Condition.java
+++ src/com/company/If_Else_Condition.java
@@ -5,13 +5,18 @@ public class If_Else_Condition {
     public static void main(String[] args) {
         System.out.println("if-else condition");
         Scanner sc = new Scanner(System.in);
+        Scanner sv = new Scanner(System.in);
         int age;
+        int check_sum;
+//        boolean bl;
 
-        int a =56;
-        int b = 96;
+        System.out.println("Enter num1: ");
+        int a =sc.nextInt();
+        System.out.println("Enter num2: ");
+        int b = sc.nextInt();
 
         if (a>b)
-        System.out.printf("%d is greater than %d", a,b);
+        System.out.printf("%d is greater than %d", a, b);
         else
             System.out.println("False");
 
@@ -21,6 +26,34 @@ public class If_Else_Condition {
             System.out.println("Eligible for Driving");
         else
             System.out.println("Not Eligible for Driving");
+
+        System.out.println("Enter check_sum: ");
+        check_sum=sc.nextInt();
+
+        if (check_sum==29)
+        {
+            System.out.println("check_sum is correct");
+        }
+        else
+        {
+            System.out.println("check_sum is false");
+        }
+
+        System.out.println("Enter a String: ");
+        String name = sv.nextLine();
+//        System.out.println(name.startsWith("A"));
+        boolean value = name.startsWith("A");
+        System.out.println(value);
+        if (value==true)
+        {
+            System.out.println("Value Matched");
+        }
+        else
+        {
+            System.out.println("Value does not match");
+        }
+
+
 
     }
 }
--- src/com/company/Switch_Case.java
+++ src/com/company/Switch_Case.java
@@ -1,4 +1,13 @@
 package com.company;
+import java.util.Scanner;
 
 public class Switch_Case {
+    public static void main(String[] args) {
+        System.out.println("Usage of Switch Case");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter String: ");
+        String name=sc.nextLine();
+        System.out.println(name);
+
+    }
 }
