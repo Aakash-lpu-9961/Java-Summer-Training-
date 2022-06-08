3b34bdc5108210e9184e19eb869637156177240e
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-08 21:12:41 +0530

    Switch_Case

--- src/com/company/Switch_Case.java
+++ src/com/company/Switch_Case.java
@@ -5,9 +5,7 @@ public class Switch_Case {
     public static void main(String[] args) {
         System.out.println("Usage of Switch Case");
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter String: ");
-        String name=sc.nextLine();
-        System.out.println(name);
+
 
     }
 }
