79c2af3d20b673c74b3e353a0e829b8b1d6aa301
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-08 19:19:27 +0530

    String_Methods

--- src/com/company/String_Methods.java
+++ src/com/company/String_Methods.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Locale;
+
 public class String_Methods {
     public static void main(String[] args) {
 //        String Methods in Java
         System.out.println("String Methods");
+        String name = "Aakash";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
     }
 }
