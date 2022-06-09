0266f726bef6083065e9c90f7f2cd7287a37a919
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-09 19:36:11 +0530

    doWhile_loop_java

---
+++ src/com/company/doWhile_loop_java.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class doWhile_loop_java {
+    public static void main(String[] args) {
+        System.out.println("do-while loop in Java");
+        int a =1;
+        do {
+            System.out.println(a);
+            a++;
+        }while (a<10);
+    }
+}
