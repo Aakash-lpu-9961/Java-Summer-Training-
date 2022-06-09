336f25f178e0c3172005f68a0c2941aea6a81901
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-09 19:19:02 +0530

    while_loop_java

---
+++ src/com/company/while_loop_java.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class while_loop_java {
+    public static void main(String[] args) {
+        System.out.println("While loops in Java");
+        int count=1;
+        while (count<=20)
+        {
+            System.out.println("This is while loop "+"\t");
+            count++;
+        }
+    }
+}
