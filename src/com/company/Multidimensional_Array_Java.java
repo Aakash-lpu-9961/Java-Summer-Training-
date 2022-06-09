b00b966774a24658ceeb81bfd0927537fc541b03
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-09 22:25:33 +0530

    Multidimensional_Array_Java

---
+++ src/com/company/Multidimensional_Array_Java.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Multidimensional_Array_Java {
+    public static void main(String[] args) {
+        System.out.println("Multidimensional Array in Java");
+    }
+}
