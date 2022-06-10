364f76ce0f5bfe73923919ecf212cdbc9156b8cc
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-11 00:40:36 +0530

    Problem-01 : Program to find the minimum and maximum element in an Array

---
+++ src/com/company/Practice01_Program_Java.java
@@ -0,0 +1,21 @@
+package com.company;
+
+// Problem-01 : Program to find the minimum and maximum element in an Array
+public class Practice01_Program_Java {
+    public static void main(String[] args) {
+        System.out.println("Practice Program Java - 01");
+//        declaring an array
+        int[] num = {12,16,84,22,11,9,6,1,18};
+        int max = num[0];
+        for (int i=1;i<num.length;i++)
+        {
+            if (num[i]>max)
+            {
+                max = num[i];
+            }
+        }
+        System.out.println("Maximum Element is : "+ max);
+
+
+    }
+}
