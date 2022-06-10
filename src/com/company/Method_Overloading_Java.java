30e2f3a5a741b8ee11790ce9069b9a9d3d3bf2c0
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-10 22:52:54 +0530

    Method_Overloading_Java

--- src/com/company/Method_Overloading_Java.java
+++ src/com/company/Method_Overloading_Java.java
@@ -1,13 +1,33 @@
 package com.company;
 
 public class Method_Overloading_Java {
-    static void telljoke()
+
+    static void foo()
     {
-        System.out.println("Hello, This is a Joke");
+        System.out.println("Good Morning Hi!!!");
+    }
+
+    static  void foo(int a)
+    {
+        System.out.println("Hi!!! Good Morning"+ a);
+    }
+
+    static void foo(int a, int b)
+    {
+        System.out.println("3rd Method: "+a+" , "+b);
     }
 
     public static void main(String[] args) {
         System.out.println("Method Overloading in Java");
-        telljoke();
-    }
+
+//        Arguments are actual
+
+        foo();
+        foo(3000);
+        foo(3000,6000);
+
+
+     }
 }
+
+//Method Overloading Cannot be performed by changing the return type of methods
