6d0ba3019d73f8d5eaebfa4a64df0ed7788fd627
Aakash-lpu-9961 <mananaakash527@gmail.com>
2022-06-10 20:54:40 +0530

    Methods2_Java

--- src/com/company/Methods2_Java.java
+++ src/com/company/Methods2_Java.java
@@ -1,7 +1,19 @@
 package com.company;
 
 public class Methods2_Java {
+
+    static void change(int [] arr)
+    {
+        arr[0]=56;
+    }
+
     public static void main(String[] args) {
+
+        int[] marks = {20,30,40,50,60,70,80,90,100};
+//        Changing the Array
+        change(marks);
+        System.out.println("The Value of x after running change is: "+ marks[0]);
+
 
     }
 }
